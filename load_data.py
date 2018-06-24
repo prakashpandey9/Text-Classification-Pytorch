176113bc087b98537b0b128c5f1cd08601cf26f3
Prakash Pandey <pandeyprakash987@gmail.com>
2018-06-25 04:49:36 +0530

    Fixed minor bug

--- load_data.py
+++ load_data.py
@@ -10,22 +10,22 @@ from torchtext import datasets
 from torchtext.vocab import Vectors, GloVe
 
 def load_dataset(test_sen=None):
-	
-	"""
-	tokenizer : Breaks sentences into a list of words. If sequential=False, no tokenization is applied
-	Field : A class that stores information about the way of preprocessing
-	fix_length : An important property of TorchText is that we can let the input to be variable length, and TorchText will
+
+    """
+    tokenizer : Breaks sentences into a list of words. If sequential=False, no tokenization is applied
+    Field : A class that stores information about the way of preprocessing
+    fix_length : An important property of TorchText is that we can let the input to be variable length, and TorchText will
                  dynamically pad each sequence to the longest sequence in that "batch". But here we are using fi_length which
                  will pad each sequence to have a fix length of 200.
                  
-	build_vocab : It will first make a vocabulary or dictionary mapping all the unique words present in the train_data to an
+    build_vocab : It will first make a vocabulary or dictionary mapping all the unique words present in the train_data to an
                   idx and then after it will use GloVe word embedding to map the index to the corresponding word embedding.
-                  
-	vocab.vectors : This returns a torch tensor of shape (vocab_size x embedding_dim) containing the pre-trained word embeddings.
-	BucketIterator : Defines an iterator that batches examples of similar lengths together to minimize the amount of padding needed.
-	
-	"""
-	
+
+    vocab.vectors : This returns a torch tensor of shape (vocab_size x embedding_dim) containing the pre-trained word embeddings.
+    BucketIterator : Defines an iterator that batches examples of similar lengths together to minimize the amount of padding needed.
+
+    """
+
     tokenize = lambda x: x.split()
     TEXT = data.Field(sequential=True, tokenize=tokenize, lower=True, include_lengths=True, batch_first=True, fix_length=200)
     LABEL = data.LabelField(tensor_type=torch.FloatTensor)
